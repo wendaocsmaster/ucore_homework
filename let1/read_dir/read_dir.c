7980891baac1ef569d4691a87e51a108ccd94efa
wendaocs <wendaocs@hotmail.com>
2023-03-06 17:25:19 +0800

    Add how to read content from a file or a directory

---
+++ let1/read_dir/read_dir.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<dirent.h>
+
+int main(int argc, char** argv)
+{
+	//Open a directory
+	DIR *curdir = opendir(argv[1]);
+	if(NULL == curdir)
+	{
+		closedir(curdir);
+		return 0;
+	}
+	// The  readdir() function returns a pointer to a dirent structure representing 
+	// the next directory entry in the directory stream pointed to by dirp.  It returns
+	// NULL on reaching the end of the directory stream or if an error occurred.
+	struct dirent *dirstruct = NULL;
+	while(dirstruct = readdir(curdir))
+	{
+		printf("%s\n",dirstruct->d_name);
+	}
+	//Close the directory.
+	closedir(curdir);
+	return 0;
+}
+
